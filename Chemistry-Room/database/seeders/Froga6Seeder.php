de3597b8d99131f9b907802d65a94bb4df8b9e24
Aitor <aitorherreraalves@gmail.com>
2022-12-21 14:11:37 +0100

    Los seeders casi terminados

--- Chemistry-Room/database/seeders/Froga6Seeder.php
+++ Chemistry-Room/database/seeders/Froga6Seeder.php
@@ -22,5 +22,110 @@ class Froga6Seeder extends Seeder
         $froga6->zhitz = '5';
         $froga6->id_pista = '6';
         $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Hennig Brand';
+        $froga6->emaitza = 'P1669';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Humphrey Davy';
+        $froga6->emaitza = 'K1807';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Humphrey Davy';
+        $froga6->emaitza = 'B1808';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Johan Gadolin ';
+        $froga6->emaitza = 'Y1794';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Andrés M. del Río';
+        $froga6->emaitza = 'V1801';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Fausto Elhuyar';
+        $froga6->emaitza = 'W1783';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Juan J. Elhuyar';
+        $froga6->emaitza = 'W1783';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Bernard Courtois';
+        $froga6->emaitza = 'I1811';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Martin H. Klaproth';
+        $froga6->emaitza = 'U1789';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Louis J. Gay-Lussac';
+        $froga6->emaitza = 'B1808';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Louis J. Thenard ';
+        $froga6->emaitza = 'B1808';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Henry Cavendish';
+        $froga6->emaitza = 'H1766';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Daniel Rutherford';
+        $froga6->emaitza = 'N1772';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Carl W. Scheele';
+        $froga6->emaitza = 'O1772';
+        $froga6->zhitz = '5';
+        $froga6->id_pista = '6';
+        $froga6->save();
+
+        $froga6 = new Froga6();
+        $froga6->izena = 'Pierre Janssen ';
+        $froga6->emaitza = 'HE1907';
+        $froga6->zhitz = '6';
+        $froga6->id_pista = '6';
+        $froga6->save();
     }
 }
