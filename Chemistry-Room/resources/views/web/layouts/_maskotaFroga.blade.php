331ef621ba4d05bfee90ec64d9041a3abdd553ea
DavidR <davidrodri1602@gmail.com>
2022-12-22 14:07:39 +0100

    LARAVEL V3.3.0

---
+++ Chemistry-Room/resources/views/web/layouts/_maskotaFroga.blade.php
@@ -0,0 +1,11 @@
+ <div class=" d-flex flex-column  position-absolute bottom-0 end-0 justify-content-end align-items-center">
+        <div class="bocadilloPista d-flex justify-content-center">
+            <h2 class="pistaText p-3 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
+                numquam excepturi natus qui in et quasi temporibus sint iste ipsam doloremque nulla veritatis assumenda
+                animi sapiente, voluptas, ut error dolor!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
+                numquam excepturi natus qui in et quasi temporibus sint iste ipsam doloremque nulla veritatis assumenda
+                animi sapiente, voluptas, ut error dolor!</h2>
+        </div>
+            {{-- <img class="bombilla" src="{{asset("multimedia/bombilla.svg")}}" alt=""> --}}
+        <img id="maskotaJuego" src="{{asset("multimedia/quimica.png")}}" alt="">
+    </div>
